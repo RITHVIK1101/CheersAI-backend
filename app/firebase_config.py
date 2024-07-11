1d314bed7ad981c883ad52b383b26d9c4cf8f768
evan <evansunde2@gmail.com>
2024-07-12 00:52:47 +0545

    firebase sdk environment fix

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,6 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
+import json
 
-cred = credentials.Certificate("/home/evan/Documents/firebase/startup-app-bb5a1-firebase-adminsdk-87b5c-14ca32614b.json")
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+cred_dict = json.loads(firebase_credentials)
+
+# cred = credentials.Certificate("/home/evan/Documents/firebase/startup-app-bb5a1-firebase-adminsdk-87b5c-14ca32614b.json")
+
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 print("firebase initialized")
